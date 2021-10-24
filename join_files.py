960603027f590a00745bf87ab15d0e57d43f6201
unknown <denilavran@yandex.ru>
2021-10-24 12:27:53 +0300

    add sort and write to result file

--- join_files.py
+++ join_files.py
@@ -7,13 +7,22 @@ def get_lens_of_files():
     info_dict = []
     for item in list_files:
         with open(item, encoding="UTF-8") as file:
-            count_str = len(file.readlines())
-            info_dict.append({'name': item,'len':count_str})
+            counter = 0
+            data = ''
+            for line in file:
+                counter = counter + 1
+                data = data + line
+            info_dict.append({'name': item,'len': counter, 'data' : data})
 
-    info_dict.sorted(info_dict['len'])
+    info_dict.sort(key = lambda d : d['len'])
     return info_dict
 
+def write_to_common_file(filename, dict_wiht_date):
+    with open(filename, "w") as file:
+        for info in dict_wiht_date:
+            file.write(info['name']+'\n')
+            file.writelines(str(info['len'])+'\n')
+            file.write(info['data']+'\n')
 
-print(get_lens_of_files())
-#def write_to_common_file(filename):
 
+write_to_common_file('com.txt',get_lens_of_files())
